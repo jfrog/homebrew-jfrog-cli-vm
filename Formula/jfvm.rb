7a32aabf84b5ba36148db686b16e5544ad6eac90
Bhanu Reddy <bhanur@jfrog.com>
2025-07-15 19:30:47 +0530

    Update jfvm.rb

--- Formula/jfvm.rb
+++ Formula/jfvm.rb
@@ -1,9 +1,9 @@
 class Jfvm < Formula
   desc "Manage multiple versions of JFrog CLI"
   homepage "https://github.com/jfrog/jfrog-cli-vm"
-  url "https://github.com/jfrog/jfrog-cli-vm/releases/download/v0.0.4/jfvm-v0.0.4.tar.gz"
-  sha256 "2faaf7b74fb041c979954156adb312b0572572f56ab72d4b6ae3afdb76f9aa70"
-  version "0.0.4"
+  url "https://github.com/jfrog/jfrog-cli-vm/releases/download/v0.0.5/jfvm-v0.0.5.tar.gz"
+  sha256 "02a0909d0fdb3c6a19cd0fb4ca091d44613f131099ffa0dc312e049646964f89"
+  version "0.0.5"
 
   def install
     bin.install "jfvm"
